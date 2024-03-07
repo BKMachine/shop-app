0a86c2376810d9771ebeaee439eac574338dea98
DBKynd <db@dbkynd.com>
2024-03-06 21:20:34 -0700

    reload app on version change

--- server/src/server/api/index.ts
+++ server/src/server/api/index.ts
@@ -1,12 +1,22 @@
 import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 import SupplierRoutes from './routes/suppliers';
 import ToolRoutes from './routes/tools';
 import VendorRoutes from './routes/vendors';
 
 const router = Router();
+const uuid = uuidv4();
 
 router.get('/', (req, res, next) => {
   res.status(200).json({ message: 'Welcome to the API' });
+});
+
+router.get('/version', (req, res, next) => {
+  try {
+    res.status(200).send(uuid);
+  } catch (e) {
+    next(e);
+  }
 });
 
 router.use(SupplierRoutes);
--- www/src/main.ts
+++ www/src/main.ts
@@ -3,7 +3,22 @@ import './assets/style.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import { registerPlugins } from './plugins';
+import axios from '@/plugins/axios';
 
 const app = createApp(App);
 registerPlugins(app);
 app.mount('#app');
+
+let version: string;
+
+async function getVersion() {
+  axios.get('/version').then(({ data }) => {
+    if (!version) version = data;
+    else if (data !== version) {
+      location.reload();
+    }
+  });
+}
+
+getVersion();
+setInterval(getVersion, 1000 * 60 * 5);
