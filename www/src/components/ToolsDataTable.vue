81dae95dd952fe7558f45cfaff1f0295205637a0
DBKynd <dave@dbkynd.com>
2024-03-19 15:22:25 -0600

    virtual data table

--- www/src/components/ToolsDataTable.vue
+++ www/src/components/ToolsDataTable.vue
@@ -22,27 +22,24 @@
           ></v-text-field>
         </template>
 
-        <v-data-table
-          v-model:page="page"
+        <v-data-table-virtual
           :headers="headers"
           :items="items"
           :search="search"
           :loading="toolStore.loading"
           @click:row="openTool"
-          @update:page="setPage"
-          @update:items-per-page="setPage"
         >
           <template v-slot:[`item.img`]="{ item }">
             <v-img :src="item.img" class="tool-img"></v-img>
           </template>
-        </v-data-table>
+        </v-data-table-virtual>
       </v-card>
     </v-card-text>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref, watch } from 'vue';
+import { ref } from 'vue';
 import router from '@/router';
 import { useToolStore } from '@/stores/tool_store';
 
@@ -54,36 +51,9 @@ defineProps<{
 
 const toolStore = useToolStore();
 const search = ref('');
-const page = ref(1);
 
 function openTool(event: unknown, { item }: { item: ToolDoc }) {
   router.push({ name: 'viewTool', params: { id: item._id } });
-}
-
-function setPage() {
-  if (!toolStore.tools.length) return;
-  router.push({ name: 'tools', query: { page: page.value } });
-}
-
-onMounted(() => {
-  getPage();
-});
-
-const loading = computed(() => toolStore.loading);
-const query = computed(() => router.currentRoute.value.query);
-watch(loading, () => {
-  if (!loading.value) getPage();
-});
-watch(query, () => {
-  getPage();
-});
-
-function getPage() {
-  const p = router.currentRoute.value.query.page;
-  const pageNum = parseInt(p as string) || 1;
-  if (!isNaN(pageNum) && pageNum !== page.value) {
-    page.value = pageNum;
-  }
 }
 </script>
 
