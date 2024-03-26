4ae1eca17d953c5b6c6126c47f1292bd1fabc4ad
DBKynd <dave@dbkynd.com>
2024-03-26 16:05:49 -0600

    move onOrder data logic

--- server/src/database/lib/tool/tool_service.ts
+++ server/src/database/lib/tool/tool_service.ts
@@ -31,6 +31,8 @@ async function update(doc: ToolDoc_Pop) {
   };
   const oldDoc = (await Tool.findById(newDoc._id)) as ToolDoc;
   if (!oldDoc) throw new Error('Missing Tool Document');
+  // Set the orderedOn date if onOrder is newly set to true
+  if (newDoc.onOrder && !oldDoc.onOrder) newDoc.orderedOn = new Date().toISOString();
   updateChecks(newDoc);
   const updatedTool = await Tool.findByIdAndUpdate(newDoc._id, newDoc, { new: true });
   emit('tool', updatedTool);
@@ -62,8 +64,6 @@ async function pick(scanCode: string): Promise<{ status: number; tool: ToolDoc_P
 }
 
 function updateChecks(tool: ToolDoc_Pop) {
-  // Set the orderedOn date if onOrder is newly set to true
-  if (tool.onOrder && !tool.onOrder) tool.orderedOn = new Date().toISOString();
   // Assume if the current stock is now greater than the reorderThreshold that the order has been fulfilled
   if (tool.reorderThreshold > 0 && tool.stock > tool.reorderThreshold) tool.onOrder = false;
 }
