fc9d9e9e816b5e0a02519691753d70268db32537
DBKynd <dave@dbkynd.com>
2024-03-28 15:15:21 -0600

    string replace label xml

--- server/src/services/dymo_service.ts
+++ server/src/services/dymo_service.ts
@@ -1,24 +1,17 @@
 import fs from 'fs';
 import path from 'path';
 import Dymo from 'dymojs';
-import xml2js from 'xml2js';
 
-const locationLabelXml = fs.readFileSync(path.join(__dirname, 'label_location.xml'));
 const dymo = new Dymo();
-const builder = new xml2js.Builder();
 
 async function printLocationLabel(data: { loc: string; pos: string }): Promise<boolean> {
-  const parsed = await xml2js.parseStringPromise(locationLabelXml);
-  parsed.DesktopLabel.DYMOLabel[0].DynamicLayoutManager[0].LabelObjects[0].TextObject[0].FormattedText[0].LineTextSpan[0].TextSpan[0].Text[0] =
-    data.pos;
-  const barcode = `Loc:${data.loc} | ${data.pos}`;
-  parsed.DesktopLabel.DYMOLabel[0].DynamicLayoutManager[0].LabelObjects[0].QRCodeObject[0].Data[0].DataString[0] =
-    barcode;
-  parsed.DesktopLabel.DYMOLabel[0].DynamicLayoutManager[0].LabelObjects[0].QRCodeObject[0].TextDataHolder[0].Value[0] =
-    barcode;
-  const xmlString = builder.buildObject(parsed);
+  const locationLabelXml = fs.readFileSync(path.join(__dirname, 'label_location.xml'), {
+    encoding: 'utf-8',
+  });
+  const qrCode = `Loc:${data.loc} | ${data.pos}`;
+  const label = locationLabelXml.replaceAll('$POSITION', data.pos).replaceAll('$QRCODE', qrCode);
   try {
-    dymo.print('DYMO Wireless', xmlString); // TODO: Determine if print success
+    dymo.print('DYMO LabelWriter Wireless 1', label);
     return true;
   } catch (e) {
     return false;
