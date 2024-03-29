81d6b6c4126c751a20cb8d0e46c713233bd8d25d
DBKynd <dave@dbkynd.com>
2024-03-29 13:41:45 -0600

    uninstall service

---
+++ printer/service_uninstall.js
@@ -0,0 +1,15 @@
+const Service = require('node-windows').Service;
+
+const svc = new Service({
+  name: 'DYMO Wireless Printer',
+  script: 'C:\\shop-app\\printer\\index.mjs',
+});
+
+// Listen for the "uninstall" event so we know when it's done.
+svc.on('uninstall', function () {
+  console.log('Uninstall complete.');
+  console.log('The service exists: ', svc.exists);
+});
+
+// Uninstall the service.
+svc.uninstall();
