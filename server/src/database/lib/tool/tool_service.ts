aaad25866d731ca265c700d0b6e4e91130e6fd75
DBKynd <db@dbkynd.com>
2024-03-30 12:15:48 -0600

    refactor tool service
    rearrange methods
    emit on add tool
    move onReorder check back to update method

--- server/src/database/lib/tool/tool_service.ts
+++ server/src/database/lib/tool/tool_service.ts
@@ -17,9 +17,19 @@ async function findByScanCode(scanCode: string) {
     .populate('supplier');
 }
 
+async function getAutoReorders() {
+  return Tool.find({
+    $expr: { $lte: ['$stock', '$reorderThreshold'] },
+    autoReorder: true,
+  })
+    .populate('vendor')
+    .populate('supplier');
+}
+
 async function add(data: ToolDoc) {
   const doc = new Tool(data);
   await doc.save();
+  emit('tool', doc);
   return doc;
 }
 
@@ -33,19 +43,11 @@ async function update(doc: ToolDoc_Pop) {
   if (!oldDoc) throw new Error('Missing Tool Document');
   // Set the orderedOn date if onOrder is newly set to true
   if (newDoc.onOrder && !oldDoc.onOrder) newDoc.orderedOn = new Date().toISOString();
-  updateChecks(newDoc);
+  // Assume if the current stock is now greater than the reorderThreshold that the order has been fulfilled
+  if (newDoc.reorderThreshold > 0 && newDoc.stock > newDoc.reorderThreshold) newDoc.onOrder = false;
   const updatedTool = await Tool.findByIdAndUpdate(newDoc._id, newDoc, { new: true });
   emit('tool', updatedTool);
   return updatedTool;
-}
-
-async function getAutoReorders() {
-  return Tool.find({
-    $expr: { $lte: ['$stock', '$reorderThreshold'] },
-    autoReorder: true,
-  })
-    .populate('vendor')
-    .populate('supplier');
 }
 
 async function pick(scanCode: string): Promise<{ status: number; tool: ToolDoc_Pop | null }> {
@@ -57,15 +59,9 @@ async function pick(scanCode: string): Promise<{ status: number; tool: ToolDoc_P
   if (!tool) return { status: 404, tool: null };
   if (tool.stock <= 0) return { status: 400, tool: tool };
   tool.stock--;
-  updateChecks(tool);
   await tool.save();
   emit('tool', tool);
   return { status: 200, tool: tool };
-}
-
-function updateChecks(tool: ToolDoc_Pop) {
-  // Assume if the current stock is now greater than the reorderThreshold that the order has been fulfilled
-  if (tool.reorderThreshold > 0 && tool.stock > tool.reorderThreshold) tool.onOrder = false;
 }
 
 export default {
