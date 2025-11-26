903719e03ec8bd4d8ec255fae5c6c8c3581e0f7e
DBKynd <db@dbkynd.com>
2025-11-26 15:51:26 -0700

    remove unused functions and imports

--- apps/status/src/machines/Haas/serial.ts
+++ apps/status/src/machines/Haas/serial.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { RemoteSerialPort } from 'remote-serial-port-client';
 import logger from '../../logger.js';
 import { haasMachines as machines } from '../index.js';
 
@@ -26,21 +25,18 @@ export function stop() {
 
 function run() {
   machines.forEach((machine, location) => {
-    const { hostname, protocol } = new URL(location);
-    const url = `${protocol}//${hostname}/index.htm`;
     const changes: Changes = new Map();
     axios
-      .get(url)
-      .then(async () => {
+      .post<string[][]>(process.env.HAAS_SERIAL_URL as string, { url: location })
+      .then(async ({ data: responses }) => {
         const state = machine.getState() as HaasState;
         const online = state.online;
         if (!online) {
           changes.set('online', true);
           changes.set('lastStateTs', new Date().toISOString());
         }
-        const responses = await serial(location);
         if (responses.join('').trim().length) changes.set('serial', JSON.stringify(responses));
-        responses.forEach((response) => {
+        responses.forEach((response, i) => {
           const command = response.shift() as HaasCommand;
           switch (command) {
             case 'MODE': {
@@ -102,47 +98,6 @@ function run() {
         }
       });
   });
-}
-
-async function serial(location: string): Promise<string[][]> {
-  const { hostname, port } = new URL(location);
-  const responses: string[][] = [];
-  const tcp: RSPC = new RemoteSerialPort({ mode: 'tcp', host: hostname, port, reconnect: false });
-  await open(tcp);
-  // https://www.haascnc.com/service/troubleshooting-and-how-to/how-to/machine-data-collection---ngc.html
-  responses.push(await send(tcp, 104)); // MODE, xxxxx
-  responses.push(await send(tcp, 303)); // LASTCYCLE, xxxxxxx
-  responses.push(await send(tcp, 500)); // PROGRAM, Oxxxxx, STATUS, PARTS, xxxxx
-  tcp.close();
-  return responses;
-}
-
-function open(tcp: RSPC): Promise<void> {
-  return new Promise((resolve, reject) => {
-    tcp.open((error) => {
-      if (error) reject(error);
-      else resolve();
-    });
-  });
-}
-
-async function send(tcp: RSPC, code: QCode): Promise<string[]> {
-  return new Promise((resolve, reject) => {
-    tcp.write(`Q${code}\r`);
-    setTimeout(() => {
-      tcp.read((error, result) => {
-        if (error) reject(error);
-        else resolve(parse(result));
-      });
-    }, 500);
-  });
-}
-
-function parse(result: Buffer) {
-  return result
-    .toString('ascii')
-    .replace(/[^0-9A-Z,]/gi, '')
-    .split(',');
 }
 
 export function parseLastCycle(text: string): number {
