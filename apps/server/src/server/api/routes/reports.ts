f3bd5b301984f52ed6c765c9ba0039854489379c
DBKynd <db@dbkynd.com>
2026-03-24 19:32:01 -0600

    biome formatt8ng

--- apps/server/src/server/api/routes/reports.ts
+++ apps/server/src/server/api/routes/reports.ts
@@ -28,7 +28,7 @@ router.post('/reports', requireKnownDevice, async (req, res, next) => {
 });
 
 router.put('/reports', requireKnownDevice, async (req, res, next) => {
-  const { data }: { data: EmailReportDoc   | undefined } = req.body;
+  const { data }: { data: EmailReportDoc | undefined } = req.body;
   if (!data) {
     res.sendStatus(400);
     return;
