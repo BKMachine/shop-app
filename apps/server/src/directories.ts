33895ae93d0202f721a63a430fd5451f6d780812
DBKynd <db@dbkynd.com>
2026-03-25 14:49:10 -0600

    remove setups folder for images

--- apps/server/src/directories.ts
+++ apps/server/src/directories.ts
@@ -9,8 +9,7 @@ export const imageDir = path.join(dataDir, 'images');
 export const tempDir = path.join(imageDir, 'temp');
 export const toolsDir = path.join(imageDir, 'tools');
 export const partsDir = path.join(imageDir, 'parts');
-export const setupsDir = path.join(imageDir, 'setups');
 
-for (const dir of [dataDir, imageDir, tempDir, toolsDir, partsDir, setupsDir]) {
+for (const dir of [dataDir, imageDir, tempDir, toolsDir, partsDir]) {
   if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
 }
