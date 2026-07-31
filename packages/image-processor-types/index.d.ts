05f2c6a362b223a528bc6e4f2b1bc1b730869fd3
DBKynd <db@dbkynd.com>
2026-07-30 22:10:04 -0600

    move image types

---
+++ packages/image-processor-types/index.d.ts
@@ -0,0 +1,47 @@
+declare global {
+  interface PrintLocationBody {
+    loc: string;
+    pos: string;
+  }
+
+  interface PrintItemBody {
+    description: string;
+    identifier: string;
+    entity: string;
+    loc: string;
+    pos: string;
+    qrText: string;
+    imageUrl?: string;
+    labelOffsetX?: number;
+    labelOffsetY?: number;
+  }
+
+  interface PrintShipmentQtyLabelRow {
+    qty: string;
+    item: string;
+    part?: string;
+    description?: string;
+  }
+
+  interface PrintShipmentQtyLabelBody {
+    title?: string;
+    subtitle?: string;
+    rows: PrintShipmentQtyLabelRow[];
+  }
+
+  interface PrintJobTravelerRow {
+    label: string;
+    value: string;
+  }
+
+  interface PrintJobTravelerBody {
+    jobNumber: number;
+    barcodeText: string;
+    partImageUrl?: string;
+    jobDetails: PrintJobTravelerRow[];
+    partDetails: PrintJobTravelerRow[];
+    operatorNotes?: string;
+  }
+}
+
+export {};
